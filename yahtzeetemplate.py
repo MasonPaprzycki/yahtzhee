40db1011231cdb91de49a2960ab2502aa268d2a3
MasonPaprzycki <93019015+MasonPaprzycki@users.noreply.github.com>
2022-04-18 16:30:09 -0600

    not done but don't edit
    if you want to change something make a new branch

--- yahtzeetemplate.py
+++ yahtzeetemplate.py
@@ -41,34 +41,35 @@ def yahtzee(player_one_roll, player_two_roll, player_bool):
     point_method = input("what method would you like to use to score your point? if you want to score by number \n"
                          "type it like '1' otherwise type by keyword \n "
                          "the keywords are yahtzeee, straight, and chance. ")
-    points = 0
+    globals()[f"player_{player(player_bool)}_points"] = 0
     if type(point_method) == 'int':
         for point_method in locals()[f"player_{player(player_bool)}_roll"]:
-            points += point_method
+            globals()[f"player_{player(player_bool)}_points"] += point_method
     elif point_method == 'yahtzee':
         valid = True
         for i in range(1,6):
-            if not locals()[f"player_{player(player_bool)}_roll"][0] == locals()[f"player_{player(player_bool)}_roll"][i]:
+            if not globals()[f"player_{player(player_bool)}_roll"][0] == locals()[f"player_{player(player_bool)}_roll"][i]:
 
                 valid = False
         if valid:
-            points += 50
+            globals()[f"player_{player(player_bool)}_points"] += 50
         else:
             print('no yahtzhee')
     elif point_method == 'straight':
         valid = True
         for i in range(1,6):
-            if not locals()[f"player_{player(player_bool)}_roll"][i] > locals()[f"player_{player(player_bool)}_roll"][i - 1]:
+            if not globals()[f"player_{player(player_bool)}_roll"][i] > globals()[f"player_{player(player_bool)}_roll"][i - 1]:
                 valid = False
         if valid:
-            points += 40
+            globals()[f"player_{player(player_bool)}_points"] += 40
         else:
             print('no straight')
     elif point_method =='chance':
         for i in range(5):
-            points += locals()[f"player_{player(player_bool)}_roll"][i]
+            globals()[f"player_{player(player_bool)}_points"] += globals()[f"player_{player(player_bool)}_roll"][i]
     else:
         print('invalid point method ')
+        
     player_bool = operator.not_(player_bool)
 
 yahtzee(player_one_roll, player_two_roll, player_bool=True)
